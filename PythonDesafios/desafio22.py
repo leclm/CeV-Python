7f045ab98307bc0e12c86724c6b7a3a6e67fb036
Letícia Lima <lelima039@gmail.com>
2020-04-01 11:53:04 -0300

    Update desafio22.py

--- PythonDesafios/desafio22.py
+++ PythonDesafios/desafio22.py
@@ -1,7 +1,7 @@
-'''
+"""
 Crie um programa que leia o nome completo de uma pessoa e mostre: a) o nome com todas as letras maiúsculas ; b) o nome
 com todas minúsculas; c) quantas letras ao todo (sem considerar espaços) e d) quantas letras tem o primeiro nome.
-'''
+"""
 nome = str(input('Qual o seu nome completo? ')).strip()  # vai tirar os espaços no início e final
 print('Seu nome com todas as letras maiúsculas fica dessa forma: {}.'.format(nome.upper()))
 print('Seu nome com todas as letras minúsculas fica dessa forma: {}.'.format(nome.lower()))
